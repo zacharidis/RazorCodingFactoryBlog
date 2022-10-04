b12d3e2e2d1b64cca92f644f10679f6f714b4261
Georgios Zacharidis <zacharidis@icloud.com>
2022-10-04 14:52:39 +0300

    DbContext Data Class created

---
+++ Data/RCFBDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorCodingFactoryBlog.Models.Domain;
+
+namespace RazorCodingFactoryBlog.Data
+{
+    public class RCFBDbContext : DbContext
+    {
+        public RCFBDbContext(DbContextOptions options) : base(options)
+        {
+        
+        }
+
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
