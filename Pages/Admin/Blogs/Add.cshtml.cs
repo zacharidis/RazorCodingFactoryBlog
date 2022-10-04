bd91aab76dc0bcd7d851909353383901dac6263b
Georgios Zacharidis <zacharidis@icloud.com>
2022-10-04 16:15:48 +0300

    Injected connsctuctor added

--- Pages/Admin/Blogs/Add.cshtml.cs
+++ Pages/Admin/Blogs/Add.cshtml.cs
@@ -1,12 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorCodingFactoryBlog.Data;
+using RazorCodingFactoryBlog.Models.ViewModels;
 
 namespace RazorCodingFactoryBlog.Pages.Admin.Blogs
 {
     public class AddModel : PageModel
     {
-      
+        private readonly RCFBDbContext rCFBDbContext;
 
+        [BindProperty]
+        public AddBlogPost  AddBlogPostRequest { get; set; }
+
+        // inject the constructor 
+
+        public AddModel(RCFBDbContext rCFBDbContext)
+        {
+            this.rCFBDbContext = rCFBDbContext;
+        }
 
         public void OnGet()
         {
