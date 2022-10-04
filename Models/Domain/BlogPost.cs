2445046f35b58e3230b78d240b0d907178040ae5
Georgios Zacharidis <zacharidis@icloud.com>
2022-10-04 14:45:39 +0300

    BlogPost and Tag Models Created

---
+++ Models/Domain/BlogPost.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+
+namespace RazorCodingFactoryBlog.Models.Domain
+{
+    public class BlogPost
+    {// all non nullable
+        public Guid Id { get; set; }    
+        public string Heading { get; set; }
+
+        public  string PageTitle { get; set; }
+
+        public string Content   { get; set; }
+        public string ShortDescription { get; set; }    
+
+        public string FeaturedImageUrl { get; set; }
+
+        public string UrlHandle { get; set; }
+
+        public DateTime PulishDate { get; set; }
+
+        public string Author { get; set; }  
+
+        public bool Visible { get; set; }
+
+
+    }
+}
---
+++ Models/Domain/Tag.cs
@@ -0,0 +1,10 @@
+﻿namespace RazorCodingFactoryBlog.Models.Domain
+{
+    public class Tag
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public Guid BlogPostId { get; set; }
+    }
+}
