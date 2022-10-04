a2911652d3cfbb7bbb4251719c702eb28966cea4
Georgios Zacharidis <zacharidis@icloud.com>
2022-10-04 16:33:47 +0300

    LIst all posts functionality added

--- Pages/Admin/Blogs/List.cshtml.cs
+++ Pages/Admin/Blogs/List.cshtml.cs
@@ -1,12 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorCodingFactoryBlog.Data;
+using RazorCodingFactoryBlog.Models.Domain;
 
 namespace RazorCodingFactoryBlog.Pages.Admin.Blogs
 {
     public class ListModel : PageModel
     {
+        private readonly RCFBDbContext rCFBDbContext;
+
+        public List<BlogPost> BlogPosts { get; set; }
+
+        public ListModel(RCFBDbContext rCFBDbContext)
+        {
+            this.rCFBDbContext = rCFBDbContext;
+        }
         public void OnGet()
         {
+
+           BlogPosts = rCFBDbContext.BlogPosts.ToList();
+
+
+
         }
     }
 }
